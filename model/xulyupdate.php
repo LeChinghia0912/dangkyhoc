a02a4b3e3913eb436002ba857141557c5660f120
LeChiNghia <nghia520414@gmail.com>
2023-07-31 23:56:24 +0700

    init project

---
+++ model/xulyupdate.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	 <meta http-equiv="refresh" content="0.5 url=http://localhost/dangkyhoc/view/monhoc_view.php" >
+	<title></title>
+</head>
+<body>
+<?php
+	$mamonhoc=$_POST['mamonhoc'];
+	$tenmonhoc = $_POST['tenmonhoc'];
+	$giangvien = $_POST['giangvien'];
+	$sotinchi = $_POST['sotinchi'];
+	$hocphi = $_POST['hocphi'];
+
+	include_once("../publish/connect.php");
+	$sql="UPDATE monhoc SET tenmonhoc='$tenmonhoc', giangvien ='$giangvien', sotinchi='$sotinchi',
+	 hocphi='$hocphi' Where mamonhoc='$mamonhoc'";
+	$kq=mysqli_query($conn,$sql);
+	if ($kq) {
+		header("http://localhost/dangkyhoc/view/monhoc_view.php");
+	
+	}
+	?>
+</body>
+</html>
